895d5f61983d6c14e9ffdcbed2633335d12f6442
rapharyanto <tinydiamondzgt@gmail.com>
2025-12-20 22:59:01 +0700

    test

--- connect.js
+++ connect.js
@@ -1,15 +1,3 @@
-// const mysql = require("mysql2"); 
-
-// const pool = mysql.createPool({
-//     host: "id-dci-web1321.main-hosting.eu",
-//     user: "u355073407_kelompok5",
-//     password: "Kelompok5bijikopi.",
-//     database: "u355073407_cs_kelompok5",
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
@@ -22,18 +10,15 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-
-// Cek koneksi untuk memastikan database bisa diakses
 pool.getConnection((err, connection) => {
     if (err) {
         console.error("❌ Database connection failed:", err.message);
     } else {
         console.log("✅ Database connected!");
-        connection.release(); // lepas koneksi kembali ke pool
+        connection.release();
     }
 });
 
-// export versi promise supaya bisa pakai async/await
 module.exports = pool.promise();
 
 
